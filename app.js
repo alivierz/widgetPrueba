794ee38eb8a5b2f204e2077869f4bdf3043793c2
azapata <azapata@pagochinchin.com>
2022-12-26 10:27:34 -0400

    new

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const element = document.getElementsByTagName('alivier')
 const element2 = document.getElementsByTagName('alivier-super-especicifico-bottom')
 addEventListener('click', (event) =>{
     if(event.target == element2[0]){
@@ -6,6 +5,6 @@ addEventListener('click', (event) =>{
     }
 })
 const data = () =>{
-    element[0].innerHTML = `<div style="position: fixed; background-color: rgba( 0, 0, 0, .3);min-height: 100%;min-width: 100%;display: flex;justify-content: center;align-items: center;top: 0;"> <h2>hola mundo</h2> </div>
-    `
+    document.body.innerHTML = `<div style="position: fixed; background-color: rgba( 0, 0, 0, .3);min-height: 100%;min-width: 100%;display: flex;justify-content: center;align-items: center;top: 0;"> <h2>hola mundo</h2> </div>`
+    ;
 }
