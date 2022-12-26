3ae3afb0ef4bb5bade08a2c6e625b48346394f00
azapata <azapata@pagochinchin.com>
2022-12-26 13:28:43 -0400

    datos

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const element2 = document.getElementsByTagName('alivier-super-especicifico-botto
 addEventListener('click', (event) =>{
     if(event.target == element2[0]){
         document.body.insertAdjacentHTML('afterbegin', `
-        <div id="chinchin-alivier" style="position: fixed; background-color: rgba( 0, 0, 0, .8);min-height: 100%;min-width: 100%;display: flex;justify-content: center;align-items: center;top: 0;z-index: 30;"> <h2>hola mundo</h2>
+        <div id="chinchin-alivier" style="position: fixed; background-color: rgba( 255, 255, 255, .8);min-height: 100%;min-width: 100%;display: flex;justify-content: center;align-items: center;top: 0;z-index: 1000;">
             <div style="height: 200px;width: 200px;display: flex;justify-content: center;align-items;">
                 <h2>HOLA MUNDO</h2>
                 <button id="btn-cerrar">Cerrar</button>
