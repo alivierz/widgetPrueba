91c84376e43302b31e3614a2d5a860a4b7d69d5d
azapata <azapata@pagochinchin.com>
2022-12-26 10:33:07 -0400

    a

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const element2 = document.getElementsByTagName('alivier-super-especicifico-bottom')
 addEventListener('click', (event) =>{
-
+    console.log(event)
     if(event.target == element2[0]){
         document.body.innerHTML = `<div style="position: fixed; background-color: rgba( 0, 0, 0, .3);min-height: 100%;min-width: 100%;display: flex;justify-content: center;align-items: center;top: 0;"> <h2>hola mundo</h2> </div>`;
     }
