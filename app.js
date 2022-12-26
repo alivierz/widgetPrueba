2d39dc31a6e34255d8032e011092b05481dacae0
azapata <azapata@pagochinchin.com>
2022-12-26 14:17:19 -0400

    similar a metamap

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const element2 = document.getElementsByTagName('alivier-super-especicifico-botto
 addEventListener('click', (event) =>{
     if(event.target == element2[0]){
         document.body.insertAdjacentHTML('afterbegin', `
-        <div id="chinchin-alivier" style="position: fixed; background-color: rgba( 147, 0, 147, .8);min-height: 100%;min-width: 100%;display: flex;justify-content: center;align-items: center;top: 0;z-index: 10000;">
-            <div style="height: 200px;width: 200px;display: flex;justify-content: center;color: white;background-color: black;flex-direction: column;
-            border-radius: 20px;align-items: center;">
-                <h2>HOLA MUNDO</h2>
+        <div id="chinchin-alivier" style="position: fixed;backdrop-filter: blur(5px);background-color: rgba(33,35,45,.9);min-height: 100%;min-width: 100%;display: flex;justify-content: center;align-items: center;top: 0;z-index: 10000;">
+            <div style="align-items: stretch;background-color: #fff;box-sizing: border-box;display: flex;flex-direction: column;overflow: hidden;padding: 20px;justify-content: center;min-height: 580px;min-width: 400px;border-radius: 20px;">
+                <h2 style="text-align: center;">HOLA MUNDO</h2>
                 <button id="btn-cerrar">Cerrar</button>
             </div>
         </div>`);
