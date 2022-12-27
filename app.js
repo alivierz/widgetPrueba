603a6c9e23c248b8b6ea4b4f6a4cb706e759336f
azapata <azapata@pagochinchin.com>
2022-12-27 16:34:23 -0400

    agregando interacciones

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ addEventListener('click', (event) =>{
         <div id="chinchin-alivier" style="position: fixed;backdrop-filter: blur(5px);background-color: rgba(33,35,45,.9);min-height: 100%;min-width: 100%;display: flex;justify-content: center;align-items: center;top: 0;z-index: 10000;">
             <div style="align-items: stretch;background-color: #fff;box-sizing: border-box;display: flex;flex-direction: column;overflow: hidden;padding: 20px;justify-content: center;min-height: 580px;min-width: 400px;border-radius: 20px;">
                 <h2 style="text-align: center;">HOLA MUNDO</h2>
-                <iframe src="https://pokedextrabajo.netlify.app/"  style="overflow: hidden; min-height: 500px; min-width: 100%;border: 1px solid black;" ></iframe>
+                <iframe src="http://localhost:4200/${element2[0].attributes[0].value}" allow-same-origin style="overflow: hidden; min-height: 500px; min-width: 100%;border: 1px solid black;" ></iframe>
                 <button id="btn-cerrar">Cerrar</button>
             </div>
         </div>`);
