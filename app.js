f9d8bb2f4f070fdcb68408b2717437da66645fcd
azapata <azapata@pagochinchin.com>
2023-01-01 18:49:08 -0400

    sdfghjk

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const element2 = document.getElementsByTagName('alivier-super-especicifico-bottom')
-
-function sendMessageAlivier() {
-    window.top.postMessage('message', '*');
-}
 //! generador de eventos
-const sendEvent = () =>{
-    window.top.postMessage('alivier', '*');
-    document.getElementById('chinchin-alivier').remove()
-}
+
 addEventListener('click', (event) =>{
     if(event.target == element2[0]){
         document.body.insertAdjacentHTML('afterbegin', `
@@ -26,6 +19,12 @@ addEventListener('click', (event) =>{
     }
 }, {passive: true})
 
+window.onmessage = (e) =>{
+    if(e.data.complete){ 
+        document.getElementById('chinchin-alivier')?.remove()
+    }
+}
+
 //Funcion que se debe usar
     
 // let button = document.getElementById('chinchin-alivier-principal')
@@ -37,20 +36,3 @@ addEventListener('click', (event) =>{
     
 //   }
 // }
-
-
-// const dataChinchinWidget = () =>{
-//     document.body.insertAdjacentHTML('afterbegin', `
-//     <div id="chinchin-alivier" style="position: fixed; background-color: rgba( 147, 0, 147, .8);min-height: 100%;min-width: 100%;display: flex;justify-content: center;align-items: center;top: 0;z-index: 10000;">
-//         <div style="height: 200px;width: 200px;display: flex;justify-content: center;color: white;background-color: black;flex-direction: column;
-//         border-radius: 20px;align-items: center;">
-//             <h2>HOLA MUNDO</h2>
-//             <button id="btn-cerrar">Cerrar</button>
-//         </div>
-//     </div>`);
-// }
-// addEventListener('click', (event2) => {
-//     if(event2.target  == document.getElementById('btn-cerrar')){
-//         document.getElementById('chinchin-alivier').remove()
-//     }
-// })
