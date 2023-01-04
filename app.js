c1ab11fbf93d4cb83ca6e741b04ba933bac11a6a
azapata <azapata@pagochinchin.com>
2023-01-04 10:37:31 -0400

    ASDFGH

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const element2 = document.getElementsByTagName('alivier-super-especicifico-bottom')
+const element2 = document.getElementsByTagName('chinchin-pay')
 //! generador de eventos
-const sendMessageAlivier = ( ) =>{
+const sendMessageChinchin = ( ) =>{
     window.top.postMessage('close-widget-chinchin')
 }
 addEventListener('click', (event) =>{
@@ -10,7 +10,7 @@ addEventListener('click', (event) =>{
             <div style="align-items: stretch;background-color: #fff;box-sizing: border-box;display: flex;flex-direction: column;overflow: hidden;padding: 20px;justify-content: center;min-height: 580px;min-width: 80%;border-radius: 20px;">
                 <h2 style="text-align: center;">Bienvenido ${element2[0].attributes['idtokenChinchin'].value}</h2>
                 <iframe src="http://localhost:4200/${element2[0].attributes['idtokenChinchin'].value}" allow-same-origin style="overflow: hidden; min-height: 500px; min-width: 100%;border: 1px solid black;" ></iframe>
-                <button id="btn-cerrar" onclick="sendMessageAlivier()">Cerrar</button>
+                <button id="btn-cerrar" onclick="sendMessageChinchin()">Cerrar</button>
             </div>
         </div>`);
     }
