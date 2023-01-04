b8682e7e89c675125009517f38cc3c6284cbed19
azapata <azapata@pagochinchin.com>
2023-01-04 10:57:53 -0400

    akjsdnasjbdkas

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const sendMessageChinchin = ( ) =>{
 addEventListener('click', (event) =>{
     if(event.target == element2[0]){
         document.body.insertAdjacentHTML('afterbegin', `
-        <div id="chinchin-alivier" style="position: fixed;backdrop-filter: blur(5px);background-color: rgba(33,35,45,.9);min-height: 100%;min-width: 100%;display: flex;justify-content: center;align-items: center;top: 0;z-index: 10000;"max-width: 100%;>
+        <div id="chinchin-alivier" style="position: fixed;backdrop-filter: blur(5px);background-color: rgba(33,35,45,.9);min-height: 100%;min-width: 100%;display: flex;justify-content: center;align-items: center;top: 0;z-index: 10000;"max-width: 1200px;padding: 20px;>
             <div style="align-items: stretch;background-color: #fff;box-sizing: border-box;display: flex;flex-direction: column;overflow: hidden;padding: 20px;justify-content: center;min-height: 580px;min-width: 80%;max-width: 80%;border-radius: 20px;">
                 <h2 style="text-align: center;">Bienvenido ${element2[0].attributes['idtokenChinchin'].value}</h2>
                 <iframe src="http://localhost:4200/${element2[0].attributes['idtokenChinchin'].value}" allow-same-origin style="overflow: hidden; min-height: 500px; min-width: 100%;border: 1px solid black;" ></iframe>
