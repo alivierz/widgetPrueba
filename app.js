f14d9090edd01e3b863b225d142db929d4c588d0
azapata <azapata@pagochinchin.com>
2023-01-16 15:25:02 -0400

    el senor de ;a noche

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ addEventListener('click', (event) =>{
             @media (max-width: 535px) {
                 .widgetChinchinDev{
                     width: 100vw!important;
-                    height: 100%!important;
+                    min-height: 100%!important;
                 }
             }
         </style>
