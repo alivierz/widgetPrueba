70bbdf3a8259e4799be49e01b8b038259d8b5406
azapata <azapata@pagochinchin.com>
2023-01-16 09:47:43 -0400

    wdadsafas

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ addEventListener('click', (event) =>{
         document.body.insertAdjacentHTML('afterbegin', `
         <div id="chinchin-alivier" style="position: fixed;backdrop-filter: blur(5px);background-color: rgba(33,35,45,.9);min-height: 100%;width: 100%;display: flex;justify-content: center;align-items: center;top: 0;z-index: 10000;">
             <div style="align-items: stretch;background-color: #fff;box-sizing: border-box;display: flex;flex-direction: column;overflow: hidden;padding: 10px;justify-content: center;min-height: 580px;width: 80%;max-width: 1000px;border-radius: 20px;">
-                <h2 style="text-align: center;">PowerByChinchin</h2>
+                <h2 style="text-align: end;" onclick="sendMessageChinchin()">X</h2>
                 <iframe src="http://localhost:4200/${element2[0].attributes['idtokenChinchin'].value}" allow-same-origin style="overflow: hidden; min-height: 500px; min-width: 100%;border: none;" ></iframe>
-                <button id="btn-cerrar" onclick="sendMessageChinchin()">Cerrar</button>
             </div>
         </div>`
         );
