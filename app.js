9e095e5b21294d59638b4c1c0f861054837ad22e
azapata <azapata@pagochinchin.com>
2023-01-25 09:19:28 -0400

    dasdasd

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ addEventListener('click', (event) =>{
         }else{
             document.body.insertAdjacentHTML('afterbegin', `
             <div id="chinchin-alivier" style="position: fixed;backdrop-filter: blur(5px);background-color: rgba(33,35,45,.9);min-height: 100%;width: 100%;display: flex;justify-content: center;align-items: center;top: 0;z-index: 10000;">
-                <div style="align-items: stretch;box-sizing: border-box;display: flex;flex-direction: column;overflow: hidden;justify-content: center;height: 90vh;max-height: 450px;width: 80%;max-width: 280px;border-radius: 20px;">
+                <div style="align-items: stretch;box-sizing: border-box;display: flex;flex-direction: column;overflow: hidden;justify-content: center;height: 90vh;max-height: 550px;width: 80%;max-width: 280px;border-radius: 20px;">
                     <iframe src="http://localhost:4200/room/${element2[0].attributes['idtokenChinchin'].value}" allow-same-origin style="overflow: hidden; min-height: 100%; min-width: 100%;border: none;border-radius: 20px;" ></iframe>
                 </div>
             </div>`
