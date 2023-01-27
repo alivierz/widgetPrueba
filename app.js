ecb73ccb5f543920007b4030576516957a603688
azapata <azapata@pagochinchin.com>
2023-01-27 09:58:26 -0400

    asdsads

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const sendMessageChinchin = ( ) =>{
     window.top.postMessage('close-widget-chinchin')
 }
 addEventListener('click', (event) =>{
-    console.log(element2[0].attributes['tab'].value)
     if(event.target == element2[0]){
-        if(true){
+        if(element2[0].attributes['target'].value != 'blank'){
             document.body.insertAdjacentHTML('afterbegin', `
             <div id="chinchin-alivier" style="position: fixed;backdrop-filter: blur(5px);background-color: rgba(33,35,45,.9);min-height: 100%;width: 100%;display: flex;justify-content: center;align-items: center;top: 0;z-index: 10000;">
                 <div style="align-items: stretch;box-sizing: border-box;display: flex;flex-direction: column;overflow: hidden;justify-content: center;height: 90vh;max-height: 650px;width: 80%;max-width: 800px;border-radius: 20px;">
