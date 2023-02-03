1f1a5e647430782554e83351736b3cdd6aaef5d8
azapata <azapata@pagochinchin.com>
2023-02-03 12:52:46 -0400

    asda

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ addEventListener('click', (event) =>{
         if(element2[0].attributes['target'].value != 'tab'){
             document.body.insertAdjacentHTML('afterbegin', `
             <div id="chinchin-alivier" style="position: fixed;backdrop-filter: blur(5px);background-color: rgba(33,35,45,.9);min-height: 100%;width: 100%;display: flex;justify-content: center;align-items: center;top: 0;z-index: 10000;">
-                <div style="align-items: stretch;box-sizing: border-box;display: flex;flex-direction: column;overflow: hidden;justify-content: center;max-height: 650px;max-width: 800px;border-radius: 20px;${height}">
+                <div style="align-items: stretch;box-sizing: border-box;display: flex;flex-direction: column;overflow: hidden;justify-content: center;max-height: 650px;max-width: 800px;border-radius: 20px;${width}">
                     <iframe src="http://localhost:4200/${element2[0].attributes['idtokenChinchin'].value}" allow-same-origin style="overflow: hidden; min-height: 100%; min-width: 100%;border: none;border-radius: 20px;" ></iframe>
                 </div>
             </div>`
