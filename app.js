76460b0b71d8cbf90a54e2d26335c644b24f3b30
azapata <azapata@pagochinchin.com>
2023-02-06 09:27:19 -0400

    adsasdasd

--- app.js
+++ app.js
@@ -15,8 +15,29 @@ addEventListener('click', (event) =>{
             );
         }else{
             document.body.insertAdjacentHTML('afterbegin', `
+            <style>
+            .chinchin-widget-style-info{
+                align-items: stretch;
+                box-sizing: border-box;
+                display: flex;
+                flex-direction: column;
+                overflow: hidden;
+                justify-content: center;
+                height: 90vh;
+                max-height: 480px;
+                width: 80%;
+                max-width: 280px;
+                border-radius: 20px;
+            }
+            @media (max-width: 700px) {
+                .chinchin-widget-style-info{
+                    min-width: 100vw!important;
+                    min-height: 100vh!important;
+                }
+            }
+            </style>
             <div id="chinchin-alivier" style="position: fixed;backdrop-filter: blur(5px);background-color: rgba(33,35,45,.9);min-height: 100%;width: 100%;display: flex;justify-content: center;align-items: center;top: 0;z-index: 10000;">
-                <div style="align-items: stretch;box-sizing: border-box;display: flex;flex-direction: column;overflow: hidden;justify-content: center;height: 90vh;max-height: 480px;width: 80%;max-width: 280px;border-radius: 20px;">
+                <div class="chinchin-widget-style-info">
                     <iframe src="http://localhost:4200/room/${element2[0].attributes['idtokenChinchin'].value}" allow-same-origin style="overflow: hidden; min-height: 100%; min-width: 100%;border: none;border-radius: 20px;" ></iframe>
                 </div>
             </div>`
